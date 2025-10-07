7cf86d5ab7cf3405dba4b9a62a428ab549c74686
joaomiguell85 <joaomiguellgomescorreia@gmail.com>
2025-10-07 19:40:29 -0300

    upando a versão do joao

--- projeto integrador/Assets/scripts/player1.cs
+++ projeto integrador/Assets/scripts/player1.cs
@@ -3,54 +3,57 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     [SerializeField]
-    private Rigidbody2D rigidbody;
+    private Rigidbody2D rb;
 
     [SerializeField]
-    private float velocidadeMovimento;
+    private float speed;
 
     [SerializeField]
     private FixedJoystick joystick;
 
     private SpriteRenderer sprite;
 
-
+    private Animator anim;
 
     private void Start()
     {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
+
     }
 
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        //movimentação
-        float horizontal = this.joystick.Horizontal;
+        //movimentaï¿½ï¿½o
+
+        float horizontal = 1;//this.joystick.Horizontal;
         float vertical = this.joystick.Vertical;
 
-        Flip(horizontal);
-        
+        Vector2 direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-        Vector2 direcao = new Vector2(horizontal, vertical);
-        direcao = direcao.normalized;
+        rb.linearVelocity = direction.normalized * speed;
+        //transform.Translate(direction.normalized * speed);
+       // rb.AddForce(direction * speed);
 
-        Debug.Log(direcao + " => " + direcao.magnitude);
-
-        this.rigidbody.linearVelocity = direcao * this.velocidadeMovimento;
-
-
-
-    }
-    //flip do sprite do personagem
-    private void Flip(float horizontal)
-    {
-        if (horizontal > 0)
+        if (direction.x != 0)
         {
-            sprite.flipX = false;
+            anim.SetLayerWeight(0, 0);
+            anim.SetLayerWeight(1, 0);
         }
-        else if (horizontal < 0)
+
+        if (direction != Vector2.zero)
         {
-            sprite.flipX = true;
+            anim.SetBool("andando", true);
         }
+        else
+        {
+            anim.SetBool("andando", false);
+        }
+
+
+
     }
 }
