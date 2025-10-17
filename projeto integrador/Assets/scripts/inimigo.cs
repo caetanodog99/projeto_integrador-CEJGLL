f48e9d3f837a47d0bb38736f6e2770ee6a6f7cd2
caetanodog99 <caetanouniso@gmail.com>
2025-10-17 16:13:15 -0300

    upando movimentacao inimigo

---
+++ projeto integrador/Assets/scripts/inimigo.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class inimigo : MonoBehaviour
+{
+
+    [SerializeField] private Transform alvo;
+    [SerializeField] private float velocidadeMovimento;
+    [SerializeField] private Rigidbody2D rigidbody;
+
+    void Update()
+    {
+        Vector2 posicaoAlvo = this.alvo.position;
+        Vector2 posicaoAtual = this.transform.position;
+        Vector2 direcao = posicaoAlvo - posicaoAtual;
+        direcao = direcao.normalized;
+
+        this.rigidbody.linearVelocity = (this.velocidadeMovimento * direcao);
+        
+    }
+}
