7cb75033a7f9aab31a4382725ae980d3694fee58
caetanodog99 <caetanouniso@gmail.com>
2025-11-07 16:30:05 -0300

    upando novo cenário do disconator e diálogos com 3 npcs

---
+++ projeto integrador/Assets/scripts/npcDialogo.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class npcDialogo : MonoBehaviour
+{
+    [SerializeField] private GameObject telaDialogoNPC;
+    [SerializeField] private GameObject telaDialogoBecca;
+    [SerializeField] private GameObject telaDialogoNPCLivro;
+    [SerializeField] private GameObject telaDialogoBeccaLivro;
+    [SerializeField] private string telaBoss;
+    [SerializeField] private AudioSource somInteracao;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void BotaoProximoBecca() 
+    {
+        somInteracao.Play();
+        telaDialogoBecca.SetActive(false);
+        telaDialogoNPC.SetActive(true);
+    }
+
+    public void BotaoProximoNPC()
+    {
+        somInteracao.Play();
+        telaDialogoNPC.SetActive(false);
+        telaDialogoBeccaLivro.SetActive(true);
+    }
+
+    public void BotaoProximoBeccaLivro()
+    {
+        somInteracao.Play();
+        telaDialogoBeccaLivro.SetActive(false);
+        telaDialogoNPCLivro.SetActive(true);
+        
+    }
+
+    public void BotaoBatalhar()
+    {
+        somInteracao.Play();
+        telaDialogoNPC.SetActive(false);
+        telaDialogoBecca.SetActive(false);
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(telaBoss);
+    }
+
+
+    public void AtivarTexto()
+    {
+        somInteracao.Play();
+        telaDialogoBecca.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            AtivarTexto();
+
+        }
+    }
+}
