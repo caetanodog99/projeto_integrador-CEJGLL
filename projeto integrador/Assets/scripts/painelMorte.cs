5da569973237ce6ed69ae739b15238c6188c057e
caetanodog99 <caetanouniso@gmail.com>
2025-11-18 15:06:13 -0300

    versão com tela de morte com animação bugada e ataques do tiranofax 100%

--- projeto integrador/Assets/scripts/painelMorte.cs
+++ projeto integrador/Assets/scripts/painelMorte.cs
@@ -4,9 +4,26 @@ public class painelMorte : MonoBehaviour
 {
     public inimigo boss;
     public player jogador;
+    [SerializeField] private GameObject painelVoceMorreu;
+    [SerializeField] private GameObject painelJoystick;
+    [SerializeField] private GameObject painelCoracoes;
+    [SerializeField] private Animator animator;
+
+    void Update()
+    {
+        if (painelVoceMorreu)
+        {
+            animator.Play("tela_morte");
+            painelJoystick.SetActive(false);
+            painelCoracoes.SetActive(false);
+        }
+    }
+    
 
     public void TentarNovamente()
     {
+        painelJoystick.SetActive(true);
+        painelCoracoes.SetActive(true);
         this.gameObject.SetActive(false);
         boss.ReiniciarVida();
         jogador.ReiniciarVida();
